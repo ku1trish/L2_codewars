9ab6e28ac035d19593102340f92dddb42e91b251
ku1trish <mikhalevich.arina245@gmail.com>
2025-10-20 10:26:09 +0300

    1

---
+++ z1.js
@@ -0,0 +1,27 @@
+
+function hasTwoCubeSums (n) {
+  let limit = Math.floor(Math.cbrt(n)) + 1;
+  let pairs = []; 
+  for (let a = 1; a <= limit; a++) {
+    for (let b = a + 1; b <= limit; b++) {
+      const sum = Math.pow(a, 3) + Math.pow(b, 3);
+      if (sum === n) {
+        pairs.push([a, b]);
+      }
+    }
+  }
+  if (pairs.length >= 2) {
+    for (let i = 0; i < pairs.length; i++) {
+      for (let j = i + 1; j < pairs.length; j++) {
+        const [a, b] = pairs[i];
+        const [c, d] = pairs[j];
+        if (a !== c && a !== d && b !== c && b !== d) {
+          return true; 
+        }
+      }
+    }
+  }
+  return false;
+}
+console.log(hasTwoCubeSums(1729)); 
+console.log(hasTwoCubeSums(42));   
